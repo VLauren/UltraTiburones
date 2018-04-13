f072898338430974660330989d06035f241484ca
LVilchez <lau.16.9@gmail.com>
2018-04-13 19:42:59 +0200

    primer comit

---
+++ Source/Ultratiburones/UltratiburonesGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UltratiburonesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULTRATIBURONES_API AUltratiburonesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
